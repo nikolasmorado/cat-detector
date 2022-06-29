add8aaecf587efb8a57d4e9f83ecfe3af8c5ba3f
nikolas morado <nikolasmorado@gmail.com>
2022-06-29 15:45:19 -0400

    Changes to main.py and data I plan to use

--- tfmodel/main.py
+++ tfmodel/main.py
@@ -1,2 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
 import numpy as np
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
